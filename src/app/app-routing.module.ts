4f2fd5da7aa7bd66cb94eecd73d0a268817260ba
Johny Aoun <johnyaoun@Johnys-MacBook-Pro.local>
2025-07-24 23:16:34 +0300

    feat: add new routes for cart, products, and orders functionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+
 const routes: Routes = [
   {
     path: '',
@@ -29,6 +30,10 @@ const routes: Routes = [
   {
     path: 'about',
     loadChildren: () => import('./pages/about/about.module').then(m => m.AboutModule)
+  },
+  {
+    path:'contact',
+    loadChildren:() => import('./pages/contact/contact.module').then(m => m.ContactModule)
   },
   {
     path: 'dashboard',
